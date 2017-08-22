cb03aba40bb56d57b687e012fb364b772a7c0805
Angelo Sica <angelo@blackbearnj.com>
2017-08-22 02:14:12 -0400

    Added Bootstrap

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Index File
+ */
+
+get_header();
+
+?>
+
+<div class="wrap">
+	<div class="content">
+		<h1>WP Gulp Boilerplate!</h1>
+		<p>A simple to use gulp boilerplate for gulp beginners and WordPress developers.</p>
+		
+	</div>
+	<!-- /.content -->
+</div>
+<!-- /.wrap -->
+
+
+
+<?php get_footer(); ?>
