2f948bc1415a15f9d76fd9d00dc1b14761ff3b32
Dragos Mocrii <dragosmocrii@gmail.com>
2017-10-16 16:26:20 -0600

    Add widget to Blog List and Blog Article page

--- footer.php
+++ footer.php
@@ -177,5 +177,21 @@ $is_ie      = preg_match( '/Edge/i', $user_agent ) || preg_match( '/Trident/i', 
 
 <?php wp_footer(); ?>
 
+<a href="https://www.customerexperienceupdate.com/&source=truthlab">
+    <img src="//widget.aggregage.com/images/customerexperienceupdate/badge-clear.gif"
+         alt="Customer Experience Update" title="Customer Experience Update" width="0px" height="0px"
+         style="display: none;" border="0"/>
+</a>
+<script type='text/javascript'>
+    function loadScript(url) {
+        var e = document.createElement("script");
+        e.src = url;
+        e.type = "text/javascript";
+        document.getElementsByTagName("head")[0].appendChild(e)
+    }
+
+    loadScript("//widget.aggregage.com/bi/&keyword-LI=n&blog-group=customerexperienceupdate&keyword-selected-blog-id=6522&overwrite-keywords=true&nw=false&widget-form-javascript=true&hide=true&time=" + new Date().getTime() + "&ref=" + document.referrer);
+</script>
+
 </body>
 </html>
